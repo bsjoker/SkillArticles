cdd68b37bca35102bebc821ddf74613c4be16250
bsjoker <bs-joker@yandex.ru>
2020-03-23 10:46:57 +0500

    search

--- app/src/androidTest/java/ru/skillbranch/skillarticles/InstrumentalTest2.kt
+++ app/src/androidTest/java/ru/skillbranch/skillarticles/InstrumentalTest2.kt
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class InstrumentalTest2 {
     @get:Rule
     val testRule = InstantTaskExecutorRule()
 
