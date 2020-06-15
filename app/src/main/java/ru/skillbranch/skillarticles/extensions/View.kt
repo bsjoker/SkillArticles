08964b7c8449540a1276535fce6be846d45fa3d5
bsjoker <bs-joker@yandex.ru>
2020-06-15 14:30:29 +0500

    hometask_7

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -2,6 +2,9 @@ package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.iterator
+import androidx.navigation.NavDestination
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 fun View.setMarginOptionally(left: Int? = null, top: Int? = null, right: Int? = null, bottom: Int? = null) {
     layoutParams<ViewGroup.MarginLayoutParams> {
@@ -19,6 +22,22 @@ fun View.setPaddingOptionally(
     bottom: Int = paddingBottom
 ) {
     setPadding(left, top, right, bottom)
+}
+
+fun BottomNavigationView.selectDestination(destination: NavDestination) {
+    for (menuItem in menu.iterator()) {
+        if (menuItem.itemId == destination.id) menuItem.isChecked = true
+    }
+}
+
+fun BottomNavigationView.selectItem(itemId: Int?){
+    itemId?: return
+    for (item in menu.iterator()) {
+        if(item.itemId == itemId) {
+            item.isChecked = true
+            break
+        }
+    }
 }
 
 inline fun <reified T : ViewGroup.LayoutParams> View.layoutParams(block: T.() -> Unit) {
